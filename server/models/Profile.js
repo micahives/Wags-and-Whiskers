17b8fe71276c3543e64411deb24a0aeb28c6dc4d
JustDesmarais <143149797+JustDesmarais@users.noreply.github.com>
2024-02-15 21:41:31 -0600

    build db models

---
+++ server/models/Profile.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const petSchema = require('./Pet');
+
+const profileSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must use a valid email address'],
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        myPets: [petSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+// hash new or updated user password
+profileSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+// compare and validate password for login
+profileSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+profileSchema.virtual('petCount').get(function () {
+    return this.myPets.length;
+});
+
+const Profile = model('Profile', profileSchema);
+
+module.exports = Profile;
