18c3ba43fba543d86e6d400518894d9addea2eaf
JustDesmarais <143149797+JustDesmarais@users.noreply.github.com>
2024-02-17 14:31:38 -0600

    add typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,17 +1,38 @@
 const typeDefs = `
-
-type Query {
-me: User
+  type Profile {
+    _id: ID
+    username: String
+    email: String
+    myPets: [Pet]
   }
 
-
-  type User {
-    _id: ID!
-    username: String!
-    email: String!
-    bookCount: Int
+  type Pet {
+    petName: String
+    isDog: Boolean
+    currentAge: Int
+    weight: Float
+    image: String
   }
 
+  type Auth {
+    token: ID!
+    profile: Profile
+  }
+
+  type Query {
+    profiles: [Profile]!
+    profile(profileId: ID!): Profile
+    me: Profile
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addProfile(username: String!, email: String!, password: String!): Auth
+    edittProfile(profileId: ID!, email: String, password: String): Profile
+    addPet(petName: String!, isDog: Boolean!, age: Int!, weight: Float, image: String): Profile
+    editPet(petId: ID!, petName, weight: Float, image: String): Profile
+    removePet(petId: ID!): Profile
+  }
 `;
 
 module.exports = typeDefs;
