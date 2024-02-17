0a2bea18d80a734fb1888cba958d092134b6c909
JustDesmarais <143149797+JustDesmarais@users.noreply.github.com>
2024-02-17 13:25:09 -0600

    add virtual to petSchema calculating current age

---
+++ server/models/Pet.js
@@ -0,0 +1,47 @@
+const { Schema } = require('mongoose');
+
+const petSchema = new Schema(
+    {
+        petName: {
+            type: String,
+            required: true,
+        },
+        isDog: {
+            type: Boolean,
+            required: true,
+        },
+        age: {
+            type: Number,
+            default: 3
+        },
+        weight: {
+            type: Number,
+        }, 
+        image: {
+            type: String,   
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        }
+    }
+);
+
+// virtual property calculating what the pet's current age should be based on original input
+// Takes the age entered when setting up the profile, and adds the difference (in weeks) between the current date and the CreatedAt date
+petSchema.virtual('currentAge').get(function() {
+    const millisecondsInWeek = 7 * 24 * 60 * 60 * 100;
+    const today = new Date();
+    const createdAt = new Date(this.createdAt);
+
+    const additionalWeeks = Math.floor((today - createdAt)/millisecondsInWeek);
+
+    return this.age + additionalWeeks
+});
+
+module.exports = petSchema;
