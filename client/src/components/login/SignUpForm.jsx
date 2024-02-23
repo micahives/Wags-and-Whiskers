73e0edc0451fcc3a7bdacd1e73868c8ae9c7243a
Micah Ives <micbenives@gmail.com>
2024-02-22 20:26:20 -0600

    Update LoginForm.jsx/SignUpForm.jsx for apollo functionality plus integrate styling

--- client/src/components/login/SignUpForm.jsx
+++ client/src/components/login/SignUpForm.jsx
@@ -1,43 +1,100 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import { ADD_PROFILE } from '../../utils/mutations';
 
-const SignUpForm = ({ onSubmit, onClose }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+import Auth from '../../utils/auth';
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmit({ email, password });
+const SignUpForm = ({ onSubmit }) => {
+  const [addProfile, { error, data }] = useMutation(ADD_PROFILE);
+  const [formState, setFormState] = useState({ email: '', password: '', username: '' });
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await addProfile({
+        variables: { ...formState },
+      });
+      Auth.login(data.addProfile.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+      username: '',
+    });
   };
 
   return (
-    <div className="bg-white max-w-md mx-auto mt-8 rounded-lg p-6 relative">
-      <form onSubmit={handleSubmit} className="bg-white max-w-md mx-auto mt-8 rounded-lg p-6">
-        <div className="mb-4">
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-400 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
+    <main className="flex justify-center my-4">
+      <div className="w-full max-w-md">
+        <div className="bg-white rounded px-8 pt-6 pb-8 mb-4">
+          <h2 className="text-center text-xl text-gray-900 font-bold mb-4">Create Account</h2>
+          {data ? (
+            <p className="text-center text-green-500 mb-4">
+              Success! You may now head <Link to="/" className="text-blue-500">back to the homepage.</Link>
+            </p>
+          ) : (
+            <form onSubmit={handleFormSubmit}>
+              <div className="mb-4">
+                <input
+                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  placeholder="Username"
+                  name="username"
+                  type="text"
+                  value={formState.username}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="mb-4">
+                <input
+                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  value={formState.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="mb-4">
+                <input
+                  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+              </div>
+              <button
+                className="w-full bg-black hover:bg-gray-600 font-bold py-2 px-4 rounded"
+                type="submit"
+              >
+                Submit
+              </button>
+            </form>
+          )}
+          {error && (
+            <div className="my-3 p-3 bg-red-500 text-white text-center">
+              {error.message}
+            </div>
+          )}
         </div>
-        <div className="mb-4">
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-400 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-black hover:bg-gray-600 font-bold py-2 px-4 rounded"
-        >
-          Sign Up
-        </button>
-      </form>
-    </div>
+      </div>
+    </main>
   );
 };
 
