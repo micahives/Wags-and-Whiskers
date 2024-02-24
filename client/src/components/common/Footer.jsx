8c548b1223619a066648aa94f4ff627d961eaba2
Micah Ives <micbenives@gmail.com>
2024-02-24 11:08:04 -0600

    Update footer content and spacing for consistency through the pages. Update nav border style

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
-  return (
-    <footer>
-      <p>
-        We are not Certified Veterinarians. Please consult your Primary Veterinarian
-        for medical pet advice.
-      </p>
-      <a href="https://github.com/micahives/supreme-adventure">GitHub Repo</a>
-    </footer>
-  );
+    return (
+        <footer className="bg-#252525 bg-opacity-50 text-white p-4 absolute bottom-0 w-full border-t border-gray-600">
+            <div className="flex justify-center items-center space-x-4">
+                {/* disclaimer */}
+                <p className="text-sm">Pet Wellness app is a tool to help track your pet's ongoing health checks, not a replacement for a veterinary professional.</p>
+                {/* github icon takes you to the project repository */}
+                <a href="https://github.com/micahives/supreme-adventure" target="_blank" rel="noopener noreferrer">
+                    <FaGithub className="text-4xl" />
+                </a>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
