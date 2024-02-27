1e66e8d3bc879631c03ae144fcc4c55de4005fff
Micah Ives <micbenives@gmail.com>
2024-02-26 21:35:14 -0600

    Add svg dog/cat, profilePage work

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -3,8 +3,6 @@ import { useState } from 'react';
 import Header from '../components/common/Header';
 import ProfileInfo from '../components/profile/ProfileInfo';
 import ProfilePicture from '../components/profile/ProfilePicture';
-import '../../src/'
-
 
 const ProfilePage = () => {
   // Demo data
@@ -14,29 +12,34 @@ const ProfilePage = () => {
     age: 5,
   };
 
-  const [ image ] = useState(null) 
+  const [image] = useState(null);
 
   return (
     <div className="h-full">
       <Header /> {/* Include the Header component */}
-      <div className="container mx-auto px-4 py-8 flex justify-center items-center">
-        <div className="mt-20"> {/* Adjust the margin-top as needed */}
-          <div className="flex justify-center items-center">
-            <div>
-              <ProfilePicture image={image} />
+      <div className="container px-4 py-12 mt-32">
+        <div className="max-w-xs mx-auto bg-white shadow-md rounded-lg overflow-hidden">
+          <div className="p-4 h-full">
+            <div className="h-64 flex items-center justify-center">
+              <div className="h-32 w-32 bg-gray-500 rounded-full overflow-hidden justify-center items-center">
+                {/* <img
+                  className="h-full w-full object-cover"
+                  src={image}
+                  alt="Profile"
+                /> */}
+              </div>
             </div>
-            <div className="ml-8">
-              <ProfileInfo {...petProfile} />
+            <div className="text-center mt-4">
+              <h2 className="text-xl text-black font-semibold">{petProfile.petName}</h2>
+              {/* Replace petProfile.petName with actual username */}
+              <p className="text-sm text-black">{petProfile.species}</p>
+              {/* Replace petProfile.species with actual email */}
             </div>
-          </div>
-          <div>
-            {/* Add Pet and list of pets */}
           </div>
         </div>
       </div>
     </div>
   );
 };
-
 
 export default ProfilePage;
