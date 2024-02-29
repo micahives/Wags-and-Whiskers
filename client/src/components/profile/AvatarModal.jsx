9ec44621c34aff31fe6aa5b88182eff5921da65c
Micah Ives <micbenives@gmail.com>
2024-02-28 21:11:16 -0600

    Add AvatarModal.jsx and update its state on the profile page

---
+++ client/src/components/profile/AvatarModal.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { X } from 'lucide-react'; // Importing the 'X' icon from lucide-react
+import greenCat from '../../assets/greencat.svg';
+import greenDog from '../../assets/greendog.svg';
+
+const AvatarModal = ({ onClose, onSelectAvatar }) => {
+  const [selectedAvatar, setSelectedAvatar] = useState(null);
+
+  const handleSelectAvatar = (avatar) => {
+    setSelectedAvatar(avatar);
+    onSelectAvatar(avatar); // Pass the selected avatar back to the parent component
+  };
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-700 bg-opacity-50">
+      <div className="relative bg-gray-700 rounded-lg p-8 w-96 h-64 flex flex-col justify-center items-center">
+        <button className="text-white absolute top-0 right-0 m-4" onClick={onClose}>
+          <X size={24} />
+        </button>
+        {selectedAvatar ? ( // Render selected avatar if one is selected
+          <img src={selectedAvatar === 'dog' ? greenDog : greenCat} alt="Selected Avatar" className="w-32 h-32 cursor-pointer" onClick={() => setSelectedAvatar(null)} />
+        ) : ( // Render avatar selection buttons if no avatar is selected
+          <>
+            <h2 className="text-2xl text-white font-semibold mb-4">Choose Avatar</h2>
+            <div className="flex justify-center space-x-4">
+              <button onClick={() => handleSelectAvatar('dog')}>
+                <img src={greenDog} alt="Dog Avatar" className="w-32 h-32 cursor-pointer" />
+              </button>
+              <button onClick={() => handleSelectAvatar('cat')}>
+                <img src={greenCat} alt="Cat Avatar" className="w-32 h-32 cursor-pointer" />
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AvatarModal;
