7d72f73f6e8bdd4b224c7ce8a82f30a7eb06e49d
Brian King <Thebeakeh@gmail.com>
2024-02-28 21:06:22 -0600

    added some polish to the wellness page

--- client/src/components/wellness/Checklist.jsx
+++ client/src/components/wellness/Checklist.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ChecklistItem = ({ text, isChecked, onChange }) => {
   return (
     <div className="m-1">
-      <input type="checkbox" checked={isChecked} onChange={onChange} />
+      <input type="checkbox" checked={isChecked} onChange={onChange}/>
       <label>{text}</label>
     </div>
   );
